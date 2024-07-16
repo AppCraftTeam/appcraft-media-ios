80d5b5c73bfe8b1878fb6663bef2a51c264f351c
moslienko <8676976+moslienko@users.noreply.github.com>
2024-07-16 15:07:36 +0700

    Support landscape orientation for photo gris, support iPad

--- Sources/ACMedia/ApplicationLayer/Configuration/Appearance/ACMediaLayout.swift
+++ Sources/ACMedia/ApplicationLayer/Configuration/Appearance/ACMediaLayout.swift
@@ -1,6 +1,6 @@
 //
 //  ACMediaLayout.swift
-//  
+//
 //
 //  Created by Pavel Moslienko on 20.06.2024.
 //
@@ -8,16 +8,41 @@
 import UIKit
 
 public struct ACMediaLayout {
-    public var cellsInRow: Int
+    public var phonePortraitCells: Int
+    public var phoneLandscapeCells: Int
+    public var padPortraitCells: Int
+    public var padLandscapeCells: Int
+    
     public var gridSpacing: CGFloat
     public var previewCardCornerRadius: CGFloat
-
+    
+    public var cellsInRow: Int {
+        switch (UIDevice.current.userInterfaceIdiom, UIDevice.current.orientation) {
+        case (.phone, .portrait), (.phone, .portraitUpsideDown):
+            return phonePortraitCells
+        case (.phone, .landscapeLeft), (.phone, .landscapeRight):
+            return phoneLandscapeCells
+        case (.pad, .portrait), (.pad, .portraitUpsideDown):
+            return padPortraitCells
+        case (.pad, .landscapeLeft), (.pad, .landscapeRight):
+            return padLandscapeCells
+        default:
+            return phonePortraitCells
+        }
+    }
+    
     public init(
-        cellsInRow: Int = 3,
+        phonePortraitCells: Int = 3,
+        phoneLandscapeCells: Int = 5,
+        padPortraitCells: Int = 5,
+        padLandscapeCells: Int = 7,
         gridSpacing: CGFloat = 5,
         previewCardCornerRadius: CGFloat = 0.0
     ) {
-        self.cellsInRow = cellsInRow
+        self.phonePortraitCells = max(phonePortraitCells, 1)
+        self.phoneLandscapeCells = max(phoneLandscapeCells, 1)
+        self.padPortraitCells = max(padPortraitCells, 1)
+        self.padLandscapeCells = max(padLandscapeCells, 1)
         self.gridSpacing = max(gridSpacing, 0)
         self.previewCardCornerRadius = max(previewCardCornerRadius, 0)
     }
